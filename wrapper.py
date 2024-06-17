c15d85b642ca7a0dd4327c8e1cfcefc12a8a0332
cromartie1020 <hrc245@gmail.com>
2024-06-17 07:25:28 -0400

    fixed wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,5 +1,5 @@
 from time import time
-
+import sys
 def make_timer(func):
     def wrapper(*args, **kargs):
         t1 = time()
@@ -8,15 +8,17 @@ def make_timer(func):
         print('Time: ',t2-t1)
         return ret_val
     return wrapper
-
+@make_timer
 def count_num(n):
     for i in range(n):
         for j in range(1000):
             #print(j)
             pass
 
-count_num=make_timer(count_num)
+#count_num=make_timer(count_num)
 count_num(33000)
+
+
 
 
         
