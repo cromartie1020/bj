5f7bf1552b237e8c60eea5e61a6ba0579ab01ef8
cromartie1020 <hrc245@gmail.com>
2024-06-17 00:36:16 -0400

    added argument.py and wrapper.py

---
+++ argument.py
@@ -0,0 +1,4 @@
+from sys import argv
+for arg in argv:
+   print (arg)
+#hypt()  argv[1] argv[2]
---
+++ wrapper.py
@@ -0,0 +1,25 @@
+from time import time
+
+def make_timer(func):
+    def wrapper(*args, **kargs):
+        t1 = time()
+        ret_val = func(*args, **kargs)
+        t2 = time()
+        print('Time: ',t2-t1)
+        return ret_val
+    return wrapper
+
+def count_num(n):
+    for i in range(n):
+        for j in range(1000):
+            #print(j)
+            pass
+
+count_num=make_timer(count_num)
+count_num(33000)
+
+
+        
+    
+
+    
