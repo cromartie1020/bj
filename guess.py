9e497d71d74193428b8e72a40103d06e308a7481
cromartie1020 <hrc245@gmail.com>
2024-07-19 09:04:39 -0400

    Final computer guessing program

--- guess.py
+++ guess.py
@@ -1,35 +1,29 @@
 from random import randint
 print("Please think of a number between 0 and 100")
-epsilon = 0  
+  
 number = randint(0,100)
 selected = 'h'
 high = 100
 low  = 0
-number = 1
+
 while selected != 'c':
-    print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
     if number > (int((high + low)/2)):
         low = int((high + low)/2)
         print('Is your secret number: ' + str(low) + '?' )
-        print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
-        epsilon = number - low
-        if epsilon == 1:
-            print('Your number is ', str(low) + '.')
-        
-        print ('h') 
+        print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. " , 'l')
+         
          
     elif number <  (int ((high + low)/2)): 
         high = int((high + low)/2)
         print('Is your secret number: ' + str(high) + '?')
-        print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
-        print('l')
-        epsilon = high - number
-        if epsilon == 1:
-            print ('Your number is ', int(high)) 
+        print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ", 'h')
+        
     else:
         selected = 'c'
         guess = int((high + low)/2)
-        print(number,guess)
+        print('Is your secret number: ' + str(guess) + '?')
+        print("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ", 'c')
+        print('Your number is ' + str(guess) + '.')
         
 
 
